52f442798cc6b1932f6fff92208f688b11605b07
Ilham KADDOURI <ilhamkaddourii@gmail.com>
2020-10-08 11:08:48 +0100

    from file to file

---
+++ src/main/java/com/example/Apachecamel/ApachecamelApplication.java
@@ -0,0 +1,23 @@
+package com.example.Apachecamel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApachecamelApplication extends RouteBuilder{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApachecamelApplication.class, args);
+	}
+
+	@Override
+	public void configure() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("let go");
+		from("file:C:/Users/Ilham/Desktop/a").to("file:C:\\Users\\Ilham\\Desktop");
+		System.out.println("end");
+		
+	}
+
+}
---
+++ src/test/java/com/example/Apachecamel/ApachecamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Apachecamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApachecamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
