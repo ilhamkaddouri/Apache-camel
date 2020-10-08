75f7cfd2325a892b5dc2fb7331de5e607fd0f9bf
Ilham KADDOURI <ilhamkaddourii@gmail.com>
2020-10-08 15:16:57 +0100

    csv processing

--- src/main/java/com/example/Apachecamel/ApachecamelApplication.java
+++ src/main/java/com/example/Apachecamel/ApachecamelApplication.java
@@ -1,5 +1,8 @@
 package com.example.Apachecamel;
 
+import java.util.Arrays;
+
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,11 +16,43 @@ public class ApachecamelApplication extends RouteBuilder{
 
 	@Override
 	public void configure() throws Exception {
-		// TODO Auto-generated method stub
+		// let's move data from a file to another 
 		System.out.println("let go");
-		from("file:C:/Users/Ilham/Desktop/a").to("file:C:/Users/Ilham/Desktop/b");
+		//moveAllFiles()
+		//moveSpecificFile("file1");
+		//moveWithBodyContent("world");
+		//processFileToCsv();
+		multiprocess();
 		System.out.println("end");
 		
 	}
-
+	
+	public void moveAllFiles() {
+		//noop is to not create the camel folder
+		from("file:source?noop=true").to("file:destinationf");
+	}
+	public void moveSpecificFile(String type) {
+		from("file:source?noop=true").filter(header(Exchange.FILE_NAME).startsWith("file1")).to("file:destinationf");
+	}
+	
+	public void moveWithBodyContent(String content) {
+		from("file:source?noop=true").filter(body().contains(content)).to("file:destinationf");
+	}
+	
+	public void processFileToCsv() {
+		from("file:source?noop=true").process(p->{
+			String body = p.getIn().getBody(String.class);
+			StringBuilder sb = new StringBuilder();
+			Arrays.stream(body.split(" ")).forEach(s->{
+				sb.append(s+",");
+			});
+			p.getIn().setBody(sb);
+		}).to("file:destinationf?fileName=names.csv");
+	}
+	public void multiprocess() {
+		from("file:source?noop=true").unmarshal().csv().split(body().tokenize(",")).choice()
+		.when(body().contains("Closed")).to("file:destinationf?fileName=close.csv")
+		.when(body().contains("Pending")).to("file:destinationf?fileName=pending.csv")
+		.when(body().contains("Interest")).to("file:destinationf?fileName=interest.csv");
+	}
 }
